ca1e98a02188b46b8c6bbc3fb7a3343cdc324250
Marusdi <marcoviadelcacciatore@gmail.com>
2022-07-24 16:54:16 +0200

    Updates

---
+++ precache-manifest.3c187c3e1d6fea494f51fe9dfde0dc34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d95eb30ca5d9145d281d",
+    "url": "/Marusdi/Chess-2.git/static/css/main.7b80c81e.chunk.css"
+  },
+  {
+    "revision": "d95eb30ca5d9145d281d",
+    "url": "/Marusdi/Chess-2.git/static/js/main.d95eb30c.chunk.js"
+  },
+  {
+    "revision": "b83813e89752d0003853",
+    "url": "/Marusdi/Chess-2.git/static/js/1.b83813e8.chunk.js"
+  },
+  {
+    "revision": "de88f3a354e565c4402e",
+    "url": "/Marusdi/Chess-2.git/static/js/runtime~main.de88f3a3.js"
+  },
+  {
+    "revision": "bcd943056d0fc45a3ffb171eb0d2d9b5",
+    "url": "/Marusdi/Chess-2.git/index.html"
+  }
+];
